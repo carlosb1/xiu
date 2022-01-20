8413631a37b8d8a3eba28b3aada3783004d144c4
carlosb1 <mcflurry0@gmail.com>
2022-01-20 21:24:43 +0100

    Created service module

--- application/xiu/src/service.rs
+++ application/xiu/src/service.rs
@@ -10,65 +10,11 @@ use {
         relay::{pull_client::PullClient, push_client::PushClient},
         rtmp::RtmpServer,
     },
-    std::env,
     tokio,
-    tokio::signal,
-    xiu::config::{config, config::Config},
+    xiu::config::config::Config,
 };
 
-//use application::logger::logger;
 use hls::rtmp_event_processor::RtmpEventProcessor;
-
-mod service;
-#[tokio::main]
-
-async fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    let cfg_path = &args[1];
-    let config = config::load(cfg_path);
-
-    match config {
-        Ok(val) => {
-            /*set log level*/
-
-            // flexi_logger::Logger::try_with_env_or_str("info")?.start()?;
-            // if let Some(log_config_value) = &val.log {
-            //     flexi_logger::Logger::try_with_env_or_str(log_config_value.level.clone())?
-            //         .start()?;
-            // }
-            if let Some(log_config_value) = &val.log {
-                env::set_var("RUST_LOG", log_config_value.level.clone());
-            } else {
-                env::set_var("RUST_LOG", "info");
-            }
-
-            // let mut builder = Builder::from_default_env();
-            // builder
-            //     .target(Target::Pipe(Box::new(logger::FileTarget::new(
-            //         logger::Rotate::Minute,
-            //         String::from("./logs"),
-            //     ))))
-            //     .init();
-
-            env_logger::init();
-
-            /*run the service*/
-            let mut serivce = Service::new(val);
-            serivce.run().await?;
-        }
-        _ => (),
-    }
-
-    // log::info!("log info...");
-    // log::warn!("log warn...");
-    // log::error!("log err...");
-    // log::trace!("log trace...");
-    // log::debug!("log debug...");
-
-    signal::ctrl_c().await?;
-    Ok(())
-}
 
 pub struct Service {
     cfg: Config,
